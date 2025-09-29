64683a0d6de249c10b491ae6169fd4210a337947
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-09-29 21:19:42 +0100

    Add files via upload

---
+++ load.py
@@ -0,0 +1,23 @@
+print("\033c\033[43;30m\nfile to load?")
+def loads(files):
+    a=[]
+    aa=[]
+    s=""
+    f1=open(files,"r")
+    s=f1.read()
+    f1.close()
+    arr=s.split("\n")
+    for n in arr:
+        n=n.strip()
+        arr2=n.split(",")
+        aa=[]
+        for nn in arr2:
+            nn=nn.strip()
+            if nn!="":
+                aa=aa+[nn]
+        a=a+[aa]    
+    return a
+
+files=input()
+aa=loads(files)
+print(aa)
